54a983a8e9aa9d33aa9808fe9a19a0e74b08d7cf
ashcode <55681942+ashcode028@users.noreply.github.com>
2020-09-13 20:36:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Ashita Boyina PHP</title>
+</head>
+<body>
+<h1>ashita19028 PHP </h1>
+<h2>MD5 cracker</h2>
+<div>This application  takes an MD5 hash of a four digit pin 
+and check all 10,000 possible four digit PINs to 
+determine the PIN.
+</div>
+<form action="tracker.php"  method="get" >
+  <input type="text" name="md5" size="40" >
+  <input type="submit" value="Crack md5"> 
+</form>
+
+
+
+</body>
+</html>
